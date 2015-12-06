c9c24503c36f95f5006f9284b00079d19adfecf8
Lakshmi Nallan <lnallan@planit.net.au>
2015-12-06 20:49:59 +1100

    ENV file
    This file contains the initial configuration setup for a Appium driver
    and also contains the code which is to be executed before and after
    every test scenario.

---
+++ features/support/env.rb
@@ -0,0 +1,40 @@
+require 'appium_lib'
+#require 'sauce_whisk'  - NOT USING IN THIS TEST
+require 'rspec'
+
+
+Before do | scenario |
+
+# Start Android driver
+  opts = {
+      caps:
+          { platformName: :android,
+            app: 'D://Lakshman/Mobile Automation Setup/Android/Android/android-sdk/app-test.apk',
+            app_package: 'com.ustwo.sample',
+            app_activity: 'com.ustwo.sample.CommitListActivity',
+            deviceName: 'Lucky A5',
+            newCommandTimeout: 60,
+            deviceReadyTimeout: 20
+          }
+  }
+  @driver = Appium::Driver.new(opts)
+  @driver.start_driver
+
+end
+
+# "after all"
+After do | scenario |
+
+  if scenario.passed?
+    @driver.screenshot("d:/Lakshman/Mobile Automation Setup/Android/Android/android-sdk/#{scenario.feature.name} - #{scenario.name}_PASS.png")
+
+  else
+    @driver.screenshot("d:/Lakshman/Mobile Automation Setup/Android/Android/android-sdk/#{scenario.feature.name} - #{scenario.name}_FAIL.png")
+
+  end
+
+  @driver.driver_quit
+
+end
+
+
